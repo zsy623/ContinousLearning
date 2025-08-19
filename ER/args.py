e0f9230c3123a53e6ae9bfd4575c0a1866929ef9
Silverbullet <124493967+zsy623@users.noreply.github.com>
2025-08-19 15:11:11 +0800

    Create args.py

---
+++ ER/args.py
@@ -0,0 +1,10 @@
+class args ():
+    def __init__(self):
+        self.buffer_size = 10000
+        self.minibatch_size = 32
+        self.batch_size = 32
+        self.lr = 0.01
+        self.beta = 1.0
+        self.n_epochs = 5
+        self.n_tasks = 5
+        self.seed = 42
